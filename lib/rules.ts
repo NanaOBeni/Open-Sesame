44dadcb80916cd1afe2c137e3fe6a8ee3acf119c
Isaac <isaacpettersson71@gmail.com>
2026-03-02 21:12:47 +0100

    Added rule 11, no API key exist at the moment

--- lib/rules.ts
+++ lib/rules.ts
@@ -53,4 +53,42 @@ export function country_exist(str: string, country: string): boolean {
 }
 
 
+//rule 11, time in seconds
+export function video_exist(str: string, time_to_match): boolean {
+    // OBS!!! Expects the id to be last in the string
+    function get_video_id(): string {
+           const last_11 = str.slice(-11);
+           return last_11;
+    }
+
+    function iso_to_sec(iso_string: string): number {
+        const match = iso_string.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
+        let time = 0;
+        if (match[1] != undefined) {
+            time += 3600 * match[1];
+        }
+        if (match[2] != undefined) {
+            time += 60 * match[2];
+        }
+        if (match[3] != undefined) {
+            time += match[3];
+        }
+
+        return time;
+    }
+
+    const video_id = get_video_id();
+
+    const url = `https://www.googleapis.com/youtube/v3/videos?id=${video_id}&part=contentDetails&key=${YOUTUBE_API_KEY}`;
+    
+    const response = await fetch(url);
+    const json = await response.json();
+
+    const time_iso_8601 = json.items[0].contentDetails.duration;
+
+    const time = iso_to_sec(time_iso_8601);
+
+    return time;
+}
+
 
